5309f596d89b75db1214ec240764b59d22feca57
Suat Karakusoglu <suat.karakusoglu@n11.com>
2017-01-01 02:30:54 +0300

    Initial Commit

---
+++ Parzlet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Parzlet
+//
+//  Created by Suat Karakusoglu on 1/1/17.
+//  Copyright © 2017 Parzlet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
